7df387516e6acaf8c398352b1ff1442a431f2aa6
DiscoDoggy <akaattarilim@gmail.com>
2023-03-08 13:38:11 -0800

    ISSUE: Foward selection always added first element it saw that had higher accuracy. FIXED:Made it so consider all accuracies before adding to list

--- main.py
+++ main.py
@@ -68,7 +68,51 @@ def feature_search(feature_vectors):
     print('Current Feature set:',current_feature_set)     
     print('best accuracy:', curr_best_accr)
 
+def new_feature_search(feature_vectors):
+    current_feature_set = []
+    max_accuracy = -1
+
+    for i in range(1, len(feature_vectors)):
+        curr_accuracy = 0
+        considered_features = []
+
+        for j in range(1, len(feature_vectors)):
+            
+            if j not in current_feature_set:
+                print(current_feature_set)
+                temp_curr_set = current_feature_set.copy()
+                temp_curr_set.append(j)
+                curr_accuracy = random_accuracy(temp_curr_set)
+                temp_curr_set_accuracy_tuple = (temp_curr_set, curr_accuracy)
+                considered_features.append(temp_curr_set_accuracy_tuple)
+
         
+        candidate_feature = get_max_accuracy_tuple(considered_features)
+       # print(candidate_feature)
+
+        if candidate_feature[1] > max_accuracy:
+            max_accuracy = candidate_feature[1]
+            current_feature_set = candidate_feature[0].copy()
+
+    print("Ending feature set:", current_feature_set)
+    print("Ending max accuracy:", max_accuracy)
+
+def get_max_accuracy_tuple(tuple_list):
+    curr_max = -1
+    curr_max_index = -1
+
+    for i in range(len(tuple_list)):
+
+        if tuple_list[i][1] > curr_max:
+            curr_max = tuple_list[i][1]
+            curr_max_index = i
+
+    if curr_max == -1:
+        return (-1,-1)
+    else:
+        return tuple_list[curr_max_index]
+
+                    
 
 
 def main():
@@ -76,7 +120,7 @@ def main():
     data_frame = read_in_data()
 
     temporary_data_list = [0,1,2,3,4,5,6,7,8,9]
-    feature_search(temporary_data_list)
+    new_feature_search(temporary_data_list)
 
 
     #print("Feature Count:", int_feature_count, ",", "Algorithm Choice", int_algo_choice)
